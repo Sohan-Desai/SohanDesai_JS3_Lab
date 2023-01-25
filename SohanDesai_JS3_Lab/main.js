bef70a71b3e2716bfc7401527b78cb7db7cf57c2
Sohan-Desai <sohandesai23@gmail.com>
2023-01-25 17:52:14 +0530

    Code formatted

--- SohanDesai_JS3_Lab/main.js
+++ SohanDesai_JS3_Lab/main.js
@@ -23,11 +23,11 @@ function getResults(query) {
 function displayResults(weather) {
     console.log(weather);
     let city = document.querySelector('.location .city');
-    city.innerText=`${weather.name}, ${weather.sys.country}`;
+    city.innerText = `${weather.name}, ${weather.sys.country}`;
 
     let now = new Date();
     let date = document.querySelector('.location .date');
-    date.innerText= dateBuilder(now);
+    date.innerText = dateBuilder(now);
 
     let temp = document.querySelector('.current .temp');
     temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
@@ -39,7 +39,7 @@ function displayResults(weather) {
     hilow.innerText = `${Math.round(weather.main.temp_min)}°c / ${Math.round(weather.main.temp_max)}°c`;
 }
 
-function dateBuilder (d){
+function dateBuilder(d) {
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
